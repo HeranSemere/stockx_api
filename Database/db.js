aebb69005765a833fef2c08b6c0ec2294a8af4b5
outThabox <heranseme@gmail.com>
2022-11-04 20:48:06 +0300

    New

--- Database/db.js
+++ Database/db.js
@@ -1,11 +1,11 @@
 const Pool=require("pg").Pool;
 
 const pool=new Pool({
-    user: "gpeflaxveneltk",
-    password: "7a667820282a2f2a0683dd581921203b97bc62dde0f5c2a4dd39876219b24d93",
-    database: "dc4ib5vtqbqp2i",
+    user: process.env.POSTGRESQL_USER,
+    password: process.env.POSTGRESQL_PASSWORD,
+    database: process.env.POSTGRESQL_DATABASE,
     port: 5432,
-    host: "ec2-44-199-22-207.compute-1.amazonaws.com",
+    host: process.env.POSTGRESQL_HOST,
     ssl: {
       rejectUnauthorized: false,
     },
