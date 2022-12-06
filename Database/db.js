a9d24aea4814f2585daa9ecf363ad5595dcd108d
outThabox <heranseme@gmail.com>
2022-12-06 11:06:31 +0300

    first commit

--- Database/db.js
+++ Database/db.js
@@ -1,22 +1,13 @@
 const Pool=require("pg").Pool;
 
-/*const pool=new Pool({
-    user: process.env.POSTGRESQL_USER,
-    password: process.env.POSTGRESQL_PASSWORD,
-    database: process.env.POSTGRESQL_DATABASE,
-    port: 5432,
-    host: process.env.POSTGRESQL_HOST,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });*/
+
 
   const pool=new Pool({
-    user: "gpeflaxveneltk",
-    password: "7a667820282a2f2a0683dd581921203b97bc62dde0f5c2a4dd39876219b24d93",
-    database: "dc4ib5vtqbqp2i",
+    user: "postgres",
+    password: "",
+    database: "",
     port: 5432,
-    host: "ec2-44-199-22-207.compute-1.amazonaws.com",
+    host: "",
     ssl: {
       rejectUnauthorized: false,
     },
